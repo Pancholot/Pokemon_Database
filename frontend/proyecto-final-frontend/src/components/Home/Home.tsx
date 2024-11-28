1cbc284ae64395479f244d3827d7601a7f309c2e
Pancholot <panchitofaller@gmail.com>
2024-11-28 03:23:38 -0600

    SIN ENV

--- frontend/proyecto-final-frontend/src/components/Home/Home.tsx
+++ frontend/proyecto-final-frontend/src/components/Home/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 const Home = ({ children }: any) => {
+
+    const navigate = useNavigate()
+    const trainerData = localStorage.getItem("token_pokemon")
+    if (!localStorage.getItem("token_pokemon")) {
+
+    }
+
     return (
-        <div>{children}</div>
+        <div>
+            {children}
+        </div>
     )
 }
 
